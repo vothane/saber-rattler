653a056d26a71333d7f8a0f04197d305cb6b8536
Thane Vo <vothane@gmail.com>
2013-10-06 12:13:07 -0600

    one, two, three ignition

---
+++ src/cognition_ignition/core.clj
@@ -0,0 +1,6 @@
+(ns cognition-ignition.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/cognition_ignition/core_test.clj
@@ -0,0 +1,7 @@
+(ns cognition-ignition.core-test
+  (:require [clojure.test :refer :all]
+            [cognition-ignition.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
