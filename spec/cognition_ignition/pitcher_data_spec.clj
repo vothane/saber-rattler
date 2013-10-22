624bc437d55b47739413d149b16409c00ab451c5
Thane Vo <vothane@gmail.com>
2013-10-21 23:44:58 -0600

    some refactoring

--- spec/cognition_ignition/pitcher_data_spec.clj
+++ spec/cognition_ignition/pitcher_data_spec.clj
@@ -3,17 +3,20 @@
         [incanter.core]
         [cognition-ignition.pitching-data]))
 
-(def data-url "http://www.brooksbaseball.net/tabs.php?player=452657&gFilt=&time=month&minmax=ci&var=gl&s_type=2&startDate=03/30/2007&endDate=10/20/2013&balls=-1&strikes=-1&b_hand=-1")
+(def ^:dynamic *data-url* "http://www.brooksbaseball.net/tabs.php?player=452657&gFilt=&time=month&minmax=ci&var=gl&s_type=2&startDate=03/30/2007&endDate=10/20/2013&balls=-1&strikes=-1&b_hand=-1")
 
-(def ds (load-data data-url))
+(def pitch-data (load-data *data-url*))
 
 (describe
+  
   "The web table scraper"
   (it "should pull column names from the table columns."
-      (should= ["Game" "Fourseam" "Sinker" "Change" "Curve" "Cutter"] (col-names ds)))
+    (should= ["Game" "Fourseam" "Sinker" "Change" "Curve" "Cutter"] (col-names pitch-data)))
+
   (it "should read eleven data rows."
-      (should= 11 (nrow ds)))
-  (it "should read three columns."
-      (should= 6 (ncol ds))))
+    (should= 11 (nrow pitch-data)))
+
+  (it "should read six columns."
+    (should= 6 (ncol pitch-data))))
 
 (run-specs)
--- src/cognition_ignition/pitching_data.clj
+++ src/cognition_ignition/pitching_data.clj
@@ -4,15 +4,22 @@
   (:use [incanter.core])
   (:import [java.net URL]))
 
+
+(defn get-headers [table]
+  (->> (html/select table [:thead :tr :th])
+       (map html/text)
+        vec))
+
+(defn get-rows [table]
+  (->> (html/select table [:tbody :tr :td])
+       (map #(html/select % [:td]))
+       (map #(map html/text %))
+       (filter seq)))
+
 (defn load-data
   [url]
   (let [html    (html/html-resource (URL. url))
         table   (html/select html [:table.table])
-        headers (->> (html/select table [:thead :tr :th])
-                     (map html/text)
-                     vec)
-        rows    (->> (html/select table [:tbody :tr])
-                     (map #(html/select % [:td]))
-                     (map #(map html/text %))
-                     (filter seq))]
+        headers (get-headers table)
+        rows    (get-rows table)]
   (dataset headers rows)))
