4c8250c8f07d0c0c46bc2a221a049db76cf60588
Thane Vo <vothane@gmail.com>
2013-10-22 21:43:16 -0600

    correctly get row data

--- src/cognition_ignition/pitching_data.clj
+++ src/cognition_ignition/pitching_data.clj
@@ -8,23 +8,23 @@
   (-> input
       string/lower-case
       (string/replace \space \-)
-      keyword))
+    keyword))
 
 (defn get-headers [table]
   (->> (html/select table [:thead :tr :th])
        (map html/text)
        (map to-keyword)
-       vec))
+    vec))
 
 (defn get-rows [table]
-  (->> (html/select table [:tbody :tr :td])
+  (->> (html/select table [:tr])
        (map #(html/select % [:td]))
        (map #(map html/text %))
-       (filter seq)))
+    (filter seq)))
 
 (defn load-data [url]
   (let [html    (html/html-resource (URL. url))
         table   (html/select html [:table.table])
         headers (get-headers table)
         rows    (get-rows table)]
-  (dataset headers rows)))
+    (dataset headers rows)))
