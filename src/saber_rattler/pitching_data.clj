88f1bc996f7c29297fc5471f50d5ae09837ba75a
Thane Vo <vothane@gmail.com>
2014-04-10 23:35:08 -0600

    make data query-able for Hadoop and Cascalog

--- src/saber_rattler/pitching_data.clj
+++ src/saber_rattler/pitching_data.clj
@@ -4,27 +4,22 @@
   (:use [incanter.core])
   (:import [java.net URL]))
 
-(defn to-keyword [input]
-  (-> input
-      string/lower-case
-      (string/replace \space \-)
-    keyword))
-
-(defn get-headers [table]
-  (->> (html/select table [:thead :tr :th])
-       (map html/text)
-       (map to-keyword)
-    vec))
-
-(defn get-rows [table]
-  (->> (html/select table [:tr])
-       (map #(html/select % [:td]))
-       (map #(map html/text %))
-    (filter seq)))
-
-(defn load-data [url]
+(defn get-game-logs [url]
   (let [html    (html/html-resource (URL. url))
         table   (html/select html [:table.table])
-        headers (get-headers table)
-        rows    (remove (fn [col] (= "Game" (first col))) (get-rows table))]
-    (dataset headers rows)))
+        data    (->> (html/select table [:tr])
+                     (map #(html/select % [:td]))
+                     (map #(map html/text %))
+                     (filter seq))]
+    data))
+
+(defn- pitch-per-type [pitch-types pitches]
+  (let [game            (first pitches)
+        pitches-of-game (rest pitches)
+        group-by-pitch  (for [pitch-type pitch-types num-pitches pitches-of-game] 
+                          (vector game pitch-type num-pitches))]
+    group-by-pitch))
+
+(defn game-log-converter [game-log pitch-types]
+  (let [pitch-categorizer (partial pitch-per-type pitch-types)]       
+    (into [] (map pitch-categorizer game-log))))
--- test/saber_rattler/pitching_data_test.clj
+++ test/saber_rattler/pitching_data_test.clj
@@ -6,16 +6,9 @@
 
 (def ^:dynamic *data-url* "http://www.brooksbaseball.net/tabs.php?player=452657&gFilt=&time=month&minmax=ci&var=gl&s_type=2&startDate=03/30/2007&endDate=10/20/2013&balls=-1&strikes=-1&b_hand=-1")
 
-(def pitch-data (load-data *data-url*))
-  
-(deftest pull-data-test
-  (testing "should pull column names from the table columns."
-    (is (= [:game :fourseam :sinker :change :curve :cutter] (col-names pitch-data)))))
+(def Jon-Lester-pitches [:fourseam :sinker :change :curve :cutter])
 
-(deftest read-rows-test 
-  (testing "should read eleven data rows."
-   (is (= 210 (nrow pitch-data)))))
+(def game-log-data (game-log-converter (get-game-logs *data-url*) Jon-Lester-pitches))
 
-(deftest read-columns-test
-  (testing "should read six columns."
-   (is (= 6 (ncol pitch-data)))))
+(?<- (stdout) [?game ?pitch ?num]
+  (game-log-data ?game ?pitch ?num) (= ?pitch ":sinker"))
