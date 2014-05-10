ab1b6ff7668133647248f656d7fe8c297a8ed3f7
Thane Vo <vothane@gmail.com>
2014-05-10 13:24:29 -0600

    better way of completely realizing a lazy seq with doall

--- test/saber_rattler/pitching_data_test.clj
+++ test/saber_rattler/pitching_data_test.clj
@@ -8,7 +8,7 @@
 (def ^:dynamic *pitch-outcomes-url* "http://www.brooksbaseball.net/tabs.php?player=452657&p_hand=-1&ppos=-1&cn=200&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=10/20/2013&balls=-1&strikes=-1&b_hand=-1")
 
 (defn make-queryable [data]
-  (reduce (fn [arr el] (apply conj arr (take (count el) el))) [] 
+  (reduce (fn [arr el] (apply conj arr (doall el))) [] 
     (map (fn [row] (map vec row)) data)))
 
 (def Jon-Lester-pitches (remove (fn [col] (= "Game" (first col))) 
