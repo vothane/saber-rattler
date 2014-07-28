46c57a2eb7554f811f90f25e975b6367e5366f7e
Thane Vo <vothane@gmail.com>
2014-07-28 00:35:28 -0600

    add query macro

--- src/saber_rattler/core.clj
+++ src/saber_rattler/core.clj
@@ -1,13 +1,12 @@
 (ns saber-rattler.core
-  (:use [incanter.core]
-        [saber-rattler.pitching-data])
-        [cascalog.api])
+  (:use [saber-rattler.pitching-data]
+        [cascalog.api]))
 
 (defn make-queryable [data]
-  (reduce (fn [arr el] (apply conj arr (doall el))) [] 
+  (reduce (fn [arr el] (apply conj arr (doall el))) []
     (map (fn [row] (map vec row)) data)))
 
-(def pitchers 
+(def pitchers
   #{{:name "Yu Darvish" :key 506433}
     {:name "Clayton Kershaw" :key 477132}
     {:name "Justin Verlander" :key 434378}
@@ -30,10 +29,16 @@
   (:key (first (clojure.set/select #(= (:name %) name) pitchers))))
 
 (defn build-url [name metric]
-  (str *url* 
-       (get-key name) 
+  (str *url*
+       (get-key name)
        "&p_hand=-1&ppos=-1&cn=200&gFilt=&time=month&minmax=ci&var="
        metric
        "&s_type=2&startDate=03/30/2007&endDate="
-       (get-date "MM") "/" (get-date "dd") "/" (get-date "yyyy")
+       (get-date "MM/dd/yyyy")
        "&balls=-1&strikes=-1&b_hand=-1"))
+
+(defmacro query? [name & body]
+  `(let [~'pitches (remove '(fn [col] (= "Game" (first col))) (headers->categories (build-url ~name "gl")))
+         ~'logs    (table->data (build-url ~name "gl"))]
+    ~@body))
+
--- test/saber_rattler/core_test.clj
+++ test/saber_rattler/core_test.clj
@@ -1,3 +1,6 @@
 (ns saber-rattler.core-test
   (:require [clojure.test :refer :all]
             [saber-rattler.core :refer :all]))
+
+(query? "Yu Darvish"
+ (is (< 0 (count logs))))
